f660fd4df15ce19b68f3bc25dc8b3f1979d46c8a
Zeyuan Hu <ferrishu3886@gmail.com>
2017-02-12 10:15:02 +0800

    Add feeds

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,9 +14,11 @@ TIMEZONE = 'Asia/Shanghai'
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
+FEED_DOMAIN = SITEURL
 
 # Blogroll
 # LINKS = (('Brian Aker', 'http://krow.net/'),
